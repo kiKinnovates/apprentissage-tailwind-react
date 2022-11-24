f3a4e18fd55b14933784a0dc3cec242d7d2aa8c2
kiKinnovates <kikinnovates.dev@gmail.com>
2022-11-24 16:48:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-[100vh] bg-slate-900">
+      <h1 className="text-lime-200 text-5xl">Hello World !!!</h1>
     </div>
   );
 }
